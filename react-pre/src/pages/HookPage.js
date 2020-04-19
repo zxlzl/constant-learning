24b8c6363b42fc234eca2c3ff1be76519bb3743e
zxlzl <384542246@qq.com>
2020-04-19 17:49:33 +0800

    hook

--- react-pre/src/pages/HookPage.js
+++ react-pre/src/pages/HookPage.js
@@ -2,16 +2,33 @@ import React, { useState, useEffect } from 'react'
 
 export default function HookPage() {
   const [count, setCount] = useState(0)
+  const [date, setDate] = useState(new Date())
   // componentDidMount 、componentDidUpdate
-  useEffect(()=>{
-    console.log('effect');
+  useEffect(() => {
+    console.log('effect count');
+    // 只需要在count发生改变执行
     document.title = `点击了${count}次`
-  })
+
+
+  }, [count])
+
+  useEffect(() => {
+    console.log('effect date');
+    
+    //只需要componentDidMount执行
+    const timer = setInterval(() => {
+      setDate(new Date())
+    }, 1000);
+
+    // componentWillUnmount
+    return ()=>clearInterval(timer)
+  },[])
   return (
     <div>
       <h3>HookPage</h3>
       <p>{count}</p>
-      <button onClick={()=>setCount(count+1)}>add</button>
+      <button onClick={() => setCount(count + 1)}>add</button>
+      <p>{date.toLocaleTimeString()}</p>
     </div>
   )
 }
