7675aaca1e75887e7a010e402b33d3b56b75e81b
zxlzl <384542246@qq.com>
2020-06-01 11:14:28 +0800

    mongoose

--- node/05/mongo/index.js
+++ node/05/mongo/index.js
@@ -17,7 +17,17 @@ app.get("/api/list", async (req, res)=>{
       .skip((page-1)*5)
       .limit(5)
       .toArray()
+    res.json({ok:1,data:{fruits,pagination:{total, page}}})
   } catch (error) {
+    console.log(error);
     
   }
 })
+
+app.get("/api/category", async (req, res)=>{
+  const col = mongo.col("fruits")
+  const data = await col.distinct('category')
+  res.json({ok:1,data})
+})
+
+app.listen(3000)
