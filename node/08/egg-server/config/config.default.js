491f9f119d440069700f1b7f8c6d92eae84b8fe1
zxlzl <384542246@qq.com>
2020-06-05 17:53:11 +0800

    egg实践

--- node/08/egg-server/config/config.default.js
+++ node/08/egg-server/config/config.default.js
@@ -13,41 +13,10 @@ module.exports = appInfo => {
   const config = exports = {};
 
   // use for cookie sign key, should change to your own and keep security
-  config.keys = appInfo.name + '_1561446865227_2139';
+  config.keys = appInfo.name + '_1591264431301_8981';
 
   // add your middleware config here
   config.middleware = [];
-
-  config.swaggerdoc = {
-    dirScanner: './app/controller',
-    apiInfo: {
-      title: '开课吧接口',
-      description: '开课吧接口 swagger-ui for egg',
-      version: '1.0.0',
-    },
-    schemes: ['http', 'https'],
-    consumes: ['application/json'],
-    produces: ['application/json'],
-    enableSecurity: false,
-    // enableValidate: true,
-    routerMap: true,
-    enable: true,
-  }
-
-  config.mongoose = {
-    url: 'mongodb://127.0.0.1:27017/egg_x',
-    options: {
-      // useMongoClient: true,
-      autoReconnect: true,
-      reconnectTries: Number.MAX_VALUE,
-      bufferMaxEntries: 0,
-    },
-  }
-  config.jwt = {
-    secret: 'Great4-M',
-    enable: true, // default is false
-    match: /^\/api/, // optional
-  }
 
   // add your user config here
   const userConfig = {
