3b247328d36e2972eece675a2e48237f78aafe68
zxlzl <384542246@qq.com>
2020-06-09 16:56:05 +0800

    cluster

--- node/10/cluster/test.js
+++ node/10/cluster/test.js
@@ -1,6 +1,7 @@
-const request = require('request')
+var request = require('request');
 setInterval(() => {
-  request('http://localhost:3000', function(error, res,body){
-    console.log('body',body)
-  })
-}, 1000);
+    request('http://localhost:3000', function (error, response, body) {
+        console.log('body:', body); // Print the HTML for the Google homepage.
+    })
+}, 1000
+)
