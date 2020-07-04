5418143ece29cd64278921cb0337b3a9523d1807
zxlzl <384542246@qq.com>
2020-07-04 11:33:13 +0800

    module

--- webpack/webpack-1/src/index.js
+++ webpack/webpack-1/src/index.js
@@ -1,3 +1,10 @@
-const json = require("./index.json");//commonJS 
-import { add } from "./other.js";//es module 
-console.log(json, add(2, 3));
+// const json = require("./index.json"); //commonJS
+// import { add } from "./other.js"; //es module
+// console.log(json, add(2, 3));
+
+
+import css from "./index.css";
+import { str } from "./b.js";
+const str2 = require("./a.js");
+console.log(css);
+
--- webpack/webpack-1/webpack.config.js
+++ webpack/webpack-1/webpack.config.js
@@ -1,18 +1,28 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  // 入口
+  // 单入口
   entry: "./src/index.js",
-  output: "./dist",
-  mode: "production",
+  // entry: ["./src/a.js","./src/b.js"],
+  // 多入口
+  // entry: {
+  //   a: "./src/a.js",
+  //     b: "./src/b.js",
+  // },
+  
+  output: {
+    path: path.resolve(__dirname, "dist"),
+  },
+  mode: "development",
   module: {
     rules: [
-      // loader 
+      // loader
       {
         test: /\.css$/,
-        use: "style-loader"
-      }
-    ]
+        use: "css-loader"
+      },
+    ],
   },
-  plugins: [new HtmlWebpackPlugin()] // 插件配置
-}
+  plugins: [new HtmlWebpackPlugin()], // 插件配置
+};
