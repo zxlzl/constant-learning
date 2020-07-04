efed6f99ea0600ee412781bb9214c665b13d2233
zxlzl <384542246@qq.com>
2020-07-04 16:14:12 +0800

    多页打包方案

--- webpack/webpack-2/webpack.config.js
+++ webpack/webpack-2/webpack.config.js
@@ -1,32 +1,108 @@
-const path = require('path')
-// const 
+const path = require("path");
+const htmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const glob = require("glob");
+// mpa 多页面打包方案
+// entry
+// htmlWebpackPlugin
+
+const setMpa = () => {
+  const entry = {};
+  const htmlWebpackPlugins = [];
+
+  const entryFiles = glob.sync(path.join(__dirname, "./src/*/index.js"));
+  console.log(entryFiles);
+  // [
+  //   '/Users/zhaoxiaoli/Learn/constant-learning/webpack/webpack-2/src/detail/index.js',
+  //   '/Users/zhaoxiaoli/Learn/constant-learning/webpack/webpack-2/src/home/index.js',
+  //   '/Users/zhaoxiaoli/Learn/constant-learning/webpack/webpack-2/src/list/index.js'
+  // ]
+
+  entryFiles.map((item,index)=>{
+    const entryFile = item
+    const match = entryFile.match(/src\/(.*)\/index\.js$/)
+    const pageName = match && match[1]
+    entry[pageName] = entryFile
+    htmlWebpackPlugins.push(new htmlWebpackPlugin({
+      template: path.join(__dirname,`./src/${pageName}/index.html`),
+      filename: `${pageName}.html`,
+      chunks:[pageName]
+    }))
+  })
+
+  return {
+    entry,
+    htmlWebpackPlugins,
+  };
+};
+
+const { entry, htmlWebpackPlugins } = setMpa();
 
 module.exports = {
-  entry: './src/index.js',
+  // entry: "./src/index.js",
+  // entry: {main:"./src/index.js",list: "./src/list.js",detail: "./src/detail.js"},
+  entry,
   output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname,'dist')
+    filename: "[name]_[hash:6].js",
+    path: path.resolve(__dirname, "dist"),
   },
   mode: "development",
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader',"css-loader"]
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.less$/,
-        use: ['style-loader',"css-loader","postcss-loader","less-loader"]
-      },{
+        use: ["style-loader", "css-loader", "postcss-loader", "less-loader"],
+      },
+      {
         test: /\.(png|gif|jpe?g|webp)$/,
         use: {
-          loader: 'file-loader',
+          // loader: 'file-loader',
+          loader: "url-loader",
           options: {
-            name: "[name]_[hash:6].[ext]"
-          }
-        }
-      }
-    ]
+            name: "[name]_[hash:6].[ext]",
+            outputPath: "images/",
+            limit: 10240, // 专为base64格式 放入bundle.js
+          },
+        },
+      },
+      {
+        test: /\.(eot|ttf|woff|woff2|svg)$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            name: "[name]_[hash:6].[ext]",
+            outputPath: "iconfont/",
+            limit: 1024, // 专为base64格式 放入bundle.js
+          },
+        },
+      },
+    ],
   },
-  plugins: []
-}
+  plugins: [
+    // new htmlWebpackPlugin({
+    //   title: "index",
+    //   filename: "index.html",
+    //   template: "./src/index.html",
+    //   chunks: ['main']
+    // }),
+    // new htmlWebpackPlugin({
+    //   title: "detail",
+    //   filename: "detail.html",
+    //   template: "./src/index.html",
+    //   chunks: ['detail','list']
+    // }),
+    // new htmlWebpackPlugin({
+    //   title: "list",
+    //   filename: "list.html",
+    //   template: "./src/index.html",
+    //   chunks: ['list']
+    // }),
+    ...htmlWebpackPlugins,
+
+    new CleanWebpackPlugin(),
+  ],
+};
