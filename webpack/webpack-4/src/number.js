90766714ba6c43bc0b7afafee88feec8a3b5aab4
zxlzl <384542246@qq.com>
2020-07-05 17:54:13 +0800

    publicPath

---
+++ webpack/webpack-4/src/number.js
@@ -0,0 +1,9 @@
+function number() {
+  // var div = document.createElement("div");
+  // div.setAttribute("id", "number");
+  // div.innerHTML = 12;
+  // document.body.appendChild(div);
+
+  console.log('number');
+}
+export default number;
