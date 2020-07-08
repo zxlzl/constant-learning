675b07d7f049f2661351e9757d13115e1e01b4e0
zxlzl <384542246@qq.com>
2020-07-09 07:42:23 +0800

    babel

--- webpack/webpack-5/webpack-simple/lib/webpack.js
+++ webpack/webpack-5/webpack-simple/lib/webpack.js
@@ -1,22 +1,30 @@
-const fs = require("fs")
+const fs = require("fs");
+const parser = require("@babel/parser");
+const traverse = require("@babel/traverse")
 
 module.exports = class Webpack {
   constructor(options) {
-    console.log(options);
-    const {entry, output} = options
-    this.entry = entry
-    this.output = output
+    const { entry, output } = options;
+    this.entry = entry;
+    this.output = output;
   }
   run() {
-      // 启动函数
-    this.parse(this.entry)
+    // 启动函数
+    this.parse(this.entry);
   }
   parse(entryFile) {
     // console.log(entryFile);
-    const content = fs.readFileSync(entryFile, 'utf-8')
-    console.log(content);
+    const content = fs.readFileSync(entryFile, "utf-8");
+    // 分析内容 得到ast
+    const ast = parser.parse(content, {
+      sourceType: "module",
+    });
+    console.log(ast.program.body);
+    traverse(ast,{
+      ImportDeclaration({node}){
+        console.log(node);
+      }
+    })
   }
-  file(){
-
-  }
+  file() {}
 };
