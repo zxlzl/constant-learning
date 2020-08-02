3f3dcdbea657ac684f2f72a420ac68cf5ef1e17f
zxlzl <384542246@qq.com>
2020-08-02 09:56:41 +0800

    微信调用服务端api

--- wx/gongzhonghao/index.js
+++ wx/gongzhonghao/index.js
@@ -2,6 +2,7 @@ const Koa = require("koa");
 const Router = require("koa-router");
 const static = require("koa-static");
 const bodyParser = require("koa-bodyparser");
+const axios = require("axios")
 const app = new Koa();
 app.use(bodyParser());
 const router = new Router();
@@ -18,6 +19,28 @@ router.all(
     return "大妞妞是我最爱的猪宝宝!" + message.Content;
   })
 );
+
+const tokenCache = {
+  access_token: '',
+  updateTime: Date.now(),
+  expires_in: 7200
+}
+
+router.get('/getToken',async ctx=>{
+  const url = `https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${conf.appid}&secret=${conf.appsecret}`
+  const res = await axios.get(url)
+  Object.assign(tokenCache,res.data,{
+    updateTime: Date.now()
+  })
+  ctx.body = res.data
+})
+
+router.get('/getFollowers',async ctx=>{
+  const url = `https://api.weixin.qq.com/cgi-bin/user/get?access_token=${tokenCache.access_token}`
+  const res = await axios.get(url)
+  console.log('getFollowers',res.data);
+  ctx.body = res.data
+})
 
 app.use(router.routes()); /*启动路由*/
 app.use(router.allowedMethods());
